0513edf0fc31793903a30780d70df5b273663814
Sebastian Umana <sebastian-alberto_26@hotmail.com>
2025-10-31 15:42:33 -0500

    footer logica y html

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  standalone: true,
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
